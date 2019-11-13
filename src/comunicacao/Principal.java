ab6f3ce9d667fcd649cd81a3fde415246b7d73d3
Matheus Pimentel <matheuspcouto70@gmail.com>
2019-11-13 17:27:57 -0300

    Input para os usuários

--- src/comunicacao/Principal.java
+++ src/comunicacao/Principal.java
@@ -1,16 +1,18 @@
 package comunicacao;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Terminal terminal = new Terminal();
 
 		boolean ativo = true;
 
 		while (ativo == true) {
+			
 			// MENU PRINCIPAL
 			Scanner sc = new Scanner(System.in);
 			System.out.println("======== CMD JAVA ========");
@@ -19,15 +21,16 @@ public class Principal {
 			System.out.println("(2) - MOSTRAR A VERSÃO ATUAL DO WINDOWS");
 			System.out.println("(3) - MOSTRAR A DATA ATUAL");
 			System.out.println("(4) - MOSTRAR A HORA ATUAL");
-			System.out.println("(5) - CRIAR UMA PASTA/DIRETÓRIO");
-			System.out.println("(6) - INSERIR TEXTO EM UM NOVO ARQUIVO");
-			System.out.println("(7) - RENOMEAR ARQUIVO");
-			System.out.println("(8) - RENOMEAR PASTA/DIRETÓRIO");
-			System.out.println("(9) - MOVER ARQUIVO PARA UM NOVO DIRETÓRIO");
-			System.out.println("(10) - DELETAR PASTA/DIRETÓRIO");
-			System.out.println("(11) - DELETAR ARQUIVO");
-			System.out.println("(12) - LIMPAR CONSOLE");
-			System.out.println("(13) - SAIR DO CMD");
+			System.out.println("(5) - MOSTRAR A VERSÃO ATUAL DO JAVA");
+			System.out.println("(6) - CRIAR UMA PASTA/DIRETÓRIO");
+			System.out.println("(7) - CRIAR E INSERIR TEXTO EM UM ARQUIVO");
+			System.out.println("(8) - RENOMEAR ARQUIVO");
+			System.out.println("(9) - RENOMEAR PASTA/DIRETÓRIO");
+			System.out.println("(10) - MOVER ARQUIVO PARA UM NOVO DIRETÓRIO");
+			System.out.println("(11) - DELETAR PASTA/DIRETÓRIO");
+			System.out.println("(12) - DELETAR ARQUIVO");
+			System.out.println("(13) - LIMPAR CONSOLE");
+			System.out.println("(14) - SAIR DO CMD");
 
 			int i = sc.nextInt();
 			sc.nextLine();
@@ -36,42 +39,46 @@ public class Principal {
 				terminal.executarComando("dir");
 				System.out.println("\n");
 				break;
-
 			case 2:
 				terminal.executarComando("ver");
+				System.out.println("\n");
 				break;
 			case 3:
 				terminal.executarComando("date /t");
+				System.out.println("\n");
 				break;
 			case 4:
 				terminal.executarComando("time /t");
+				System.out.println("\n");
 				break;
 			case 5:
-				terminal.executarComando("mkdir pasta1");
+				terminal.executarComando("java --version");
 				break;
 			case 6:
-				terminal.executarComando("echo 'NOVO ARQUIVO ADICIONADO' > teste1.txt");
+				terminal.criarDiretorio();
 				break;
 			case 7:
-				terminal.executarComando("rename teste1.txt teste15.txt");
+				terminal.criarArquivo();
 				break;
 			case 8:
-				terminal.executarComando("rename pasta1 pasta15");
+				terminal.renomearArquivo();
 				break;
 			case 9:
-				terminal.executarComando(
-						"move teste15.txt \"C:\\Users\\mathe\\OneDrive\\Documentos\\UNITINS\\SISTEMAS OPERACIONAIS\" ");
+				terminal.renomearDiretorio();
 				break;
 			case 10:
-				terminal.executarComando("rmdir pasta1");
+				terminal.moverArquivo();
 				break;
 			case 11:
-				terminal.executarComando("del teste1.txt");
+				terminal.deletarDiretorio();
 				break;
 			case 12:
-				terminal.limparConsole();
+				terminal.deletarArquivo();
 				break;
 			case 13:
+				terminal.limparConsole();
+				break;
+			case 14:
 				ativo = false;
 			default:
 				break;
--- src/comunicacao/Terminal.java
+++ src/comunicacao/Terminal.java
@@ -1,19 +1,116 @@
 package comunicacao;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Terminal {
-	
-public void limparConsole() {
-	for (int i = 0; i < 1000; i++) {
-		System.out.println("");
+
+	String pasta;
+	String arquivo;
+	String textoInserido;
+	String arquivoRenomeado;
+	String pastaRenomeada;
+	String arquivoMovido;
+
+	Scanner sc = new Scanner(System.in);
+
+	public void limparConsole() {
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("");
+		}
 	}
-}
-	
-public void executarComando(String comando) {
-		
+
+	public void criarDiretorio() {
+		System.out.println("Digite o nome da pasta: \n");
+		pasta = sc.nextLine();
+		File file = new File(pasta);
+		file.mkdir();
+		System.out.println("\n Pasta criada com sucesso! \n");
+	}
+
+	public void criarArquivo() throws IOException {
+		System.out.printf("Digite o nome do arquivo: \n");
+		arquivo = sc.nextLine();
+
+		FileWriter arq = new FileWriter(arquivo);
+		System.out.println("\n");
+		System.out.println("Insira o texto: \n");
+		PrintWriter gravarArq = new PrintWriter(arq);
+		textoInserido = sc.nextLine();
+
+		gravarArq.printf(textoInserido);
+		System.out.println("\n Arquivo criado com sucesso! \n");
+		arq.close();
+	}
+
+	public void renomearArquivo() {
+
+		System.out.println("Digite o nome do arquivo que deseja renomear: \n");
+		arquivo = sc.nextLine();
+		System.out.println("\n Digite o novo nome do arquivo: \n");
+		arquivoRenomeado = sc.nextLine();
+
+		new File(arquivo).renameTo(new File(arquivoRenomeado));
+		System.out.println("\n Arquivo Renomeado com sucesso! \n");
+	}
+
+	public void renomearDiretorio() {
+
+		System.out.println("Digite o nome da pasta que deseja renomear: \n");
+		pasta = sc.nextLine();
+		System.out.println("\n Digite o novo nome do pasta: \n");
+		pastaRenomeada = sc.nextLine();
+
+		new File(pasta).renameTo(new File(pastaRenomeada));
+		System.out.println("\n Pasta Renomeado com sucesso! \n");
+
+	}
+
+	public void moverArquivo() {
+
+		System.out.println("Digite o nome do arquivo que deseja mover: \n");
+		arquivo = sc.nextLine();
+		System.out.println("\n Insira o novo destino do arquivo : \n");
+		arquivoMovido = sc.nextLine();
+
+		// Arquivo que será movido
+		File moverArquivo = new File(arquivo);
+
+		// Diretorio de destino
+		File diretorioDestino = new File(arquivoMovido);
+
+		moverArquivo.renameTo(new File(diretorioDestino, arquivo));
+		System.out.println("Arquivo movido para '" + diretorioDestino.getAbsolutePath() + "'" + "\n");
+
+	}
+
+	public void deletarDiretorio() {
+
+		System.out.println("Digite o nome da pasta que deseja deletar: \n");
+		pasta = sc.nextLine();
+
+		File pastaDeletada = new File(pasta);
+		pastaDeletada.delete();
+		System.out.println("\n Pasta deletada com sucesso! \n");
+	}
+
+	public void deletarArquivo() {
+
+		System.out.println("Digite o nome do arquivo que deseja deletar: \n");
+		arquivo = sc.nextLine();
+
+		File arquivoDeletado = new File(arquivo);
+		arquivoDeletado.delete();
+		System.out.println("\n Arquivo deletado com sucesso! \n");
+	}
+
+	public void executarComando(String comando) {
+
 		ProcessBuilder processBuilder = new ProcessBuilder();
 		// Windows
 		processBuilder.command("cmd.exe", "/c", comando);
