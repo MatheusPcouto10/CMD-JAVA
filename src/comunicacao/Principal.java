d9bacf049b1a0cb8b277c505f601411cb1d185e7
Matheus Pimentel <matheuspcouto70@gmail.com>
2019-11-14 18:03:23 -0300

    Usuário precisa informar o diretório que está usando

--- src/comunicacao/Principal.java
+++ src/comunicacao/Principal.java
@@ -18,20 +18,21 @@ public class Principal {
 			System.out.println("======== CMD JAVA ========");
 			System.out.println("ESCOLHA O COMANDO: ");
 			System.out.println("(1) - LISTAR TODOS OS DIRETÓRIOS");
-			System.out.println("(2) - MOSTRAR A VERSÃO ATUAL DO WINDOWS");
-			System.out.println("(3) - MOSTRAR A VERSÃO ATUAL DO JAVA");
-			System.out.println("(4) - MOSTRAR AS PROPRIEDADES DE CONEXÃO");
-			System.out.println("(5) - MOSTRAR A DATA ATUAL");
-			System.out.println("(6) - MOSTRAR A HORA ATUAL");
-			System.out.println("(7) - CRIAR UMA PASTA/DIRETÓRIO");
-			System.out.println("(8) - CRIAR E INSERIR TEXTO EM UM ARQUIVO");
-			System.out.println("(9) - RENOMEAR ARQUIVO");
-			System.out.println("(10) - RENOMEAR PASTA/DIRETÓRIO");
-			System.out.println("(11) - MOVER ARQUIVO PARA UM NOVO DIRETÓRIO");
-			System.out.println("(12) - DELETAR PASTA/DIRETÓRIO");
-			System.out.println("(13) - DELETAR ARQUIVO");
-			System.out.println("(14) - LIMPAR CONSOLE");
-			System.out.println("(15) - SAIR DO CMD");
+			System.out.println("(2) - ACESSAR UM DIRETÓRIO");
+			System.out.println("(3) - MOSTRAR A VERSÃO ATUAL DO WINDOWS");
+			System.out.println("(4) - MOSTRAR A VERSÃO ATUAL DO JAVA");
+			System.out.println("(5) - MOSTRAR AS PROPRIEDADES DE CONEXÃO");
+			System.out.println("(6) - MOSTRAR A DATA ATUAL");
+			System.out.println("(7) - MOSTRAR A HORA ATUAL");
+			System.out.println("(8) - CRIAR UMA PASTA/DIRETÓRIO");
+			System.out.println("(9) - CRIAR E INSERIR TEXTO EM UM ARQUIVO");
+			System.out.println("(10) - RENOMEAR ARQUIVO");
+			System.out.println("(11) - RENOMEAR PASTA/DIRETÓRIO");
+			System.out.println("(12) - MOVER ARQUIVO PARA UM NOVO DIRETÓRIO");
+			System.out.println("(13) - DELETAR PASTA/DIRETÓRIO");
+			System.out.println("(14) - DELETAR ARQUIVO");
+			System.out.println("(15) - LIMPAR CONSOLE");
+			System.out.println("(16) - SAIR DO CMD");
 
 			int i = sc.nextInt();
 			sc.nextLine();
@@ -41,50 +42,54 @@ public class Principal {
 				System.out.println("\n");
 				break;
 			case 2:
-				terminal.executarComando("ver");
+				terminal.acessarDiretorio();
 				System.out.println("\n");
 				break;
 			case 3:
-				terminal.executarComando("java --version");
+				terminal.executarComando("ver");
 				System.out.println("\n");
 				break;
 			case 4:
-				terminal.executarComando("ipconfig");
+				terminal.executarComando("java --version");
 				System.out.println("\n");
 				break;
 			case 5:
-				terminal.executarComando("date /t");
+				terminal.executarComando("ipconfig");
 				System.out.println("\n");
 				break;
 			case 6:
-				terminal.executarComando("time /t");
+				terminal.executarComando("date /t");
 				System.out.println("\n");
 				break;
 			case 7:
-				terminal.criarDiretorio();
+				terminal.executarComando("time /t");
+				System.out.println("\n");
 				break;
 			case 8:
-				terminal.criarArquivo();
+				terminal.criarDiretorio();
 				break;
 			case 9:
-				terminal.renomearArquivo();
+				terminal.criarArquivo();
 				break;
 			case 10:
-				terminal.renomearDiretorio();
+				terminal.renomearArquivo();
 				break;
 			case 11:
-				terminal.moverArquivo();
+				terminal.renomearDiretorio();
 				break;
 			case 12:
-				terminal.deletarDiretorio();
+				terminal.moverArquivo();
 				break;
 			case 13:
-				terminal.deletarArquivo();
+				terminal.deletarDiretorio();
 				break;
 			case 14:
-				terminal.limparConsole();
+				terminal.deletarArquivo();
 				break;
 			case 15:
+				terminal.limparConsole();
+				break;
+			case 16:
 				ativo = false;
 			default:
 				break;
--- src/comunicacao/Terminal.java
+++ src/comunicacao/Terminal.java
@@ -2,7 +2,6 @@ package comunicacao;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -10,13 +9,16 @@ import java.util.Scanner;
 
 public class Terminal {
 
+	/* Variáveis */
 	String pasta;
 	String arquivo;
 	String textoInserido;
 	String arquivoRenomeado;
 	String pastaRenomeada;
 	String arquivoMovido;
+	String diretorioAcessado;
 
+	ProcessBuilder processBuilder = new ProcessBuilder();
 	Scanner sc = new Scanner(System.in);
 
 	public void limparConsole() {
@@ -25,93 +27,161 @@ public class Terminal {
 		}
 	}
 
-	public void criarDiretorio() {
-		System.out.println("Digite o nome da pasta: \n");
-		pasta = sc.nextLine();
-		File file = new File(pasta);
-		file.mkdir();
-		System.out.println("\n Pasta criada com sucesso! \n");
+	public void acessarDiretorio() throws IOException {
+		
+		System.out.println("Cole aqui o caminho do diretório que deseja acessar: \n");
+		diretorioAcessado = sc.nextLine();
+
+		java.awt.Desktop.getDesktop().open(new File(diretorioAcessado)); // abre no explorer a pasta de acordo com o
+																			// caminho inserido
+
+	}
+
+	public void criarDiretorio() throws IOException {
+		
+		if (diretorioAcessado != null) {
+			System.out.println("Digite o nome da pasta: \n"); // Nome da pasta que será criada
+			pasta = sc.nextLine();
+
+			// Criando a pasta
+			File file = new File(diretorioAcessado, pasta);
+			file.mkdir();
+			System.out.println("\nPasta criada com sucesso! \n");
+
+			java.awt.Desktop.getDesktop().open(new File(diretorioAcessado)); // abre no explorer a pasta de acordo com o
+																				// caminho inserido
+		} else
+			System.out.println("Por favor informe um diretório para começar! \n");
+
 	}
 
 	public void criarArquivo() throws IOException {
-		System.out.printf("Digite o nome do arquivo: \n");
-		arquivo = sc.nextLine();
+		
+		if (diretorioAcessado != null) {
+			System.out.printf("Digite o nome do arquivo: \n");// Nome do arquivo que será criado
+			arquivo = sc.nextLine();
 
-		FileWriter arq = new FileWriter(arquivo);
-		System.out.println("\n");
-		System.out.println("Insira o texto: \n");
-		PrintWriter gravarArq = new PrintWriter(arq);
-		textoInserido = sc.nextLine();
+			// Criando o arquivo
+			File file = new File(diretorioAcessado, arquivo);
+			file.createNewFile();
 
-		gravarArq.printf(textoInserido);
-		System.out.println("\n Arquivo criado com sucesso! \n");
-		arq.close();
-	}
+			// Inserindo o texto dentro do arquivo
+			System.out.println("\n");
+			System.out.println("Insira o texto: \n");
+			PrintWriter gravarArq = new PrintWriter(file);
+			textoInserido = sc.nextLine();
 
-	public void renomearArquivo() {
+			gravarArq.printf(textoInserido);
 
-		System.out.println("Digite o nome do arquivo que deseja renomear: \n");
-		arquivo = sc.nextLine();
-		System.out.println("\n Digite o novo nome do arquivo: \n");
-		arquivoRenomeado = sc.nextLine();
+			System.out.println("\nArquivo criado com sucesso! \n");
+			gravarArq.close();
 
-		new File(arquivo).renameTo(new File(arquivoRenomeado));
-		System.out.println("\n Arquivo Renomeado com sucesso! \n");
-	}
-
-	public void renomearDiretorio() {
-
-		System.out.println("Digite o nome da pasta que deseja renomear: \n");
-		pasta = sc.nextLine();
-		System.out.println("\n Digite o novo nome do pasta: \n");
-		pastaRenomeada = sc.nextLine();
-
-		new File(pasta).renameTo(new File(pastaRenomeada));
-		System.out.println("\n Pasta Renomeado com sucesso! \n");
+			java.awt.Desktop.getDesktop().open(new File(diretorioAcessado)); // abre no explorer a pasta de acordo com o
+																				// caminho inserido
+		} else
+			System.out.println("Por favor informe um diretório para começar! \n");
 
 	}
 
-	public void moverArquivo() {
+	public void renomearArquivo() throws IOException {
+		
+		if (diretorioAcessado != null) {
+			System.out.println("Digite o nome do arquivo que deseja renomear: \n");// Arquivo a ser renomeado
+			arquivo = sc.nextLine();
+			System.out.println("\nDigite o novo nome do arquivo: \n");// Novo nome que será atribuido ao arquivo
+			arquivoRenomeado = sc.nextLine();
 
-		System.out.println("Digite o nome do arquivo que deseja mover: \n");
-		arquivo = sc.nextLine();
-		System.out.println("\n Cole aqui o destino do arquivo : \n");
-		arquivoMovido = sc.nextLine();
+			// Renomeando o arquivo
+			new File(diretorioAcessado, arquivo).renameTo(new File(diretorioAcessado, arquivoRenomeado));
+			System.out.println("\nArquivo Renomeado com sucesso! \n");
 
-		// Arquivo que será movido
-		File moverArquivo = new File(arquivo);
+			java.awt.Desktop.getDesktop().open(new File(diretorioAcessado)); // abre no explorer a pasta de acordo com o
+																				// caminho inserido
+		} else
+			System.out.println("Por favor informe um diretório para começar! \n");
+	}
 
-		// Diretorio de destino
-		File diretorioDestino = new File(arquivoMovido);
+	public void renomearDiretorio() throws IOException {
+		
+		if (diretorioAcessado != null) {
+			System.out.println("Digite o nome da pasta que deseja renomear: \n");// Pasta a ser renomeada
+			pasta = sc.nextLine();
+			System.out.println("\nDigite o novo nome do pasta: \n");// Novo nome que será atribuido a pasta
+			pastaRenomeada = sc.nextLine();
 
-		moverArquivo.renameTo(new File(diretorioDestino, arquivo));
-		System.out.println("Arquivo movido para '" + diretorioDestino.getAbsolutePath() + "'" + "\n");
+			// Renomeando a pasta
+			new File(diretorioAcessado, pasta).renameTo(new File(diretorioAcessado, pastaRenomeada));
+			System.out.println("\nPasta Renomeado com sucesso! \n");
+
+			java.awt.Desktop.getDesktop().open(new File(diretorioAcessado)); // abre no explorer a pasta de acordo com o
+																				// caminho inserido
+		} else
+			System.out.println("Por favor informe um diretório para começar! \n");
 
 	}
 
-	public void deletarDiretorio() {
+	public void moverArquivo() throws IOException {
+		
+		if (diretorioAcessado != null) {
+			System.out.println("Digite o nome do arquivo que deseja mover: \n");// Arquivo a ser movido
+			arquivo = sc.nextLine();
+			System.out.println("\nCole aqui o destino do arquivo : \n");// Caminho para onde o arquivo deverá ir
+			arquivoMovido = sc.nextLine();
 
-		System.out.println("Digite o nome da pasta que deseja deletar: \n");
-		pasta = sc.nextLine();
+			// Arquivo que será movido
+			File moverArquivo = new File(diretorioAcessado, arquivo);
 
-		File pastaDeletada = new File(pasta);
-		pastaDeletada.delete();
-		System.out.println("\n Pasta deletada com sucesso! \n");
+			// Diretorio de destino
+			File diretorioDestino = new File(arquivoMovido);
+
+			// Movendo o Arquivo para o diretório de Destino
+			moverArquivo.renameTo(new File(diretorioDestino, arquivo));
+			System.out.println("\nArquivo movido para '" + diretorioDestino.getAbsolutePath() + "'" + "\n");
+
+			java.awt.Desktop.getDesktop().open(new File(diretorioDestino.getAbsolutePath())); // abre no explorer a
+																								// pasta de
+																								// acordo com o caminho
+																								// inserido
+		} else
+			System.out.println("Por favor informe um diretório para começar! \n");
+
 	}
 
-	public void deletarArquivo() {
+	public void deletarDiretorio() throws IOException {
+		
+		if (diretorioAcessado != null) {
+			System.out.println("Digite o nome da pasta que deseja deletar: \n");
+			pasta = sc.nextLine();
 
-		System.out.println("Digite o nome do arquivo que deseja deletar: \n");
-		arquivo = sc.nextLine();
+			File pastaDeletada = new File(diretorioAcessado, pasta);
+			pastaDeletada.delete();
+			System.out.println("\nPasta deletada com sucesso! \n");
 
-		File arquivoDeletado = new File(arquivo);
-		arquivoDeletado.delete();
-		System.out.println("\n Arquivo deletado com sucesso! \n");
+			java.awt.Desktop.getDesktop().open(new File(diretorioAcessado)); // abre no explorer a pasta de acordo com o
+																				// caminho inserido
+		} else
+			System.out.println("Por favor informe um diretório para começar! \n");
+	}
+
+	public void deletarArquivo() throws IOException {
+		
+		if (diretorioAcessado != null) {
+			System.out.println("Digite o nome do arquivo que deseja deletar: \n");
+			arquivo = sc.nextLine();
+
+			File arquivoDeletado = new File(diretorioAcessado, arquivo);
+			arquivoDeletado.delete();
+			System.out.println("\nArquivo deletado com sucesso! \n");
+
+			java.awt.Desktop.getDesktop().open(new File(diretorioAcessado)); // abre no explorer a pasta de acordo com o
+																				// caminho inserido
+
+		} else
+			System.out.println("Por favor informe um diretório para começar! \n");
 	}
 
 	public void executarComando(String comando) {
 
-		ProcessBuilder processBuilder = new ProcessBuilder();
 		// Windows
 		processBuilder.command("cmd.exe", "/c", comando);
 
